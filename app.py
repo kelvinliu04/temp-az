34df6798cc664bf9f4aae06ed60221194e6ba211
Kelvin Liu <kelvinliu04@gmail.com>
2020-10-07 23:45:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-import uuid
 from flask import Flask, session, redirect, url_for
+from flask_session import Session
 
 
-from datetime import datetime, timedelta
-import requests
-import msal
-import app_config
 
 app = Flask(__name__)
 
