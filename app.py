5a254441480f04847b73803644cb3e6353cd497f
Kelvin Liu <kelvinliu04@gmail.com>
2020-10-07 21:16:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,9 +68,7 @@ def _load_cache():
     if session.get("token_cache"):
         cache.deserialize(session["token_cache"])
     return cache
+    
 def _save_cache(cache):
     if cache.has_state_changed:
         session["token_cache"] = cache.serialize()
-
-if __name__ == "__main__":
-    app.run()
