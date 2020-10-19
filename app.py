bfdd14a01ba0b41a5b3153599d0fe028a9ba8ab2
Kelvin Liu <kelvinliu04@gmail.com>
2020-10-19 09:55:10 +0700

    test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import uuid
-from flask import Flask, session, redirect, url_for, request, render_template
+from flask import Flask, session, redirect, url_for, request
 from flask_session import Session
 import json
 from datetime import datetime, timedelta
@@ -9,55 +9,15 @@ import app_config
 import threading
 
 app = Flask(__name__)
-app.config.from_object(app_config)
-Session(app)
 
-from werkzeug.middleware.proxy_fix import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
-
-@app.route('/hw')
+@app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route("/")
-def index():
-    if not session.get("user"):
-        return redirect(url_for("login"))
-    return render_template('index.html', user=session["user"], version=msal.__version__)
-
-@app.route("/login")
+@app.route('/login')
 def login():
-    session["state"] = str(uuid.uuid4())
-    # Technically we could use empty list [] as scopes to do just sign in,
-    # here we choose to also collect end user consent upfront
-    auth_url = _build_auth_url(scopes=app_config.SCOPE, state=session["state"])
-    return render_template("login.html", auth_url=auth_url, version=msal.__version__)
+    return 'login'
 
-@app.route(app_config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
-def authorized():
-    if request.args.get('state') != session.get("state"):
-        return redirect(url_for("index"))  # No-OP. Goes back to Index page
-    if "error" in request.args:  # Authentication/Authorization failure
-        return render_template("auth_error.html", result=request.args)
-    if request.args.get('code'):
-        cache = _load_cache()
-        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
-            request.args['code'],
-            scopes=app_config.SCOPE,  # Misspelled scope would cause an HTTP 400 error here
-            redirect_uri=url_for("authorized", _external=True))
-        if "error" in result:
-            return render_template("auth_error.html", result=result)
-        session["user"] = result.get("id_token_claims")
-        _save_cache(cache)
-    return redirect(url_for("index"))
-
-
-@app.route("/logout")
-def logout():
-    session.clear()  # Wipe out user and its token cache from session
-    return redirect(  # Also logout from your tenant's web session
-        app_config.AUTHORITY + "/oauth2/v2.0/logout" +
-        "?post_logout_redirect_uri=" + url_for("index", _external=True))
 
 @app.route('/onlinemeeting')
 def onlinemeeting():
@@ -136,9 +96,8 @@ def _send_button_qiscus(email, name, room_id):
     
     
 def _teams_start():
-    #token = _get_token_from_pw()
-    token = _get_token_from_cache(app_config.SCOPE)
-    #print(token)
+    token = _get_token_from_pw()
+    print(token)
     if not token:
         return redirect(url_for("login"))
     
@@ -147,7 +106,7 @@ def _teams_start():
     endDT = startDT + timedelta(minutes=duration)
 
     graph_data = requests.post(  
-        "https://graph.microsoft.com/v1.0/me/onlineMeetings",
+        "https://graph.microsoft.com/v1.0/38a590da-aaa3-45e7-9e28-c2019c983894/onlineMeetings",
         headers={'Authorization': 'Bearer ' + token['access_token'],
                  'Content-type':'application/json'},
         
@@ -169,8 +128,7 @@ def _teams_start():
     return graph_data
 
 def _teams_event():
-    #token = _get_token_from_pw()
-    token = _get_token_from_cache(app_config.SCOPE)
+    token = _get_token_from_pw()
     if not token:
         return redirect(url_for("login"))
     graph_data = requests.post(  
@@ -190,29 +148,6 @@ def _teams_event():
         ).json()
     return graph_data
     
-#----------------------------------------------------------------------------------------------
-def _get_token_from_cache(scope=None):
-    cache = _load_cache()  # This web app maintains one cache per session
-    cca = _build_msal_app(cache=cache)
-    accounts = cca.get_accounts()
-    if accounts:  # So all account(s) belong to the current signed-in user
-        result = cca.acquire_token_silent(scope, account=accounts[0])
-        _save_cache(cache)
-        return result
-    
-def _build_msal_app(cache=None, authority=None):
-    return msal.ConfidentialClientApplication(
-        app_config.CLIENT_ID, authority=authority or app_config.AUTHORITY,
-        client_credential=app_config.CLIENT_SECRET, token_cache=cache)
-
-
-def _build_auth_url(authority=None, scopes=None, state=None):
-    return _build_msal_app(authority=authority).get_authorization_request_url(
-        scopes or [],
-        state=state or str(uuid.uuid4()),
-        redirect_uri=url_for("authorized", _external=True))
-
-app.jinja_env.globals.update(_build_auth_url=_build_auth_url)  # Used in template
 
                   
 if __name__ == "__main__":
